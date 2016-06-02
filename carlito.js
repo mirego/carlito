1cce7c070ca5bbca98afbad6b6f48006bf01acd0
Luc-Olivier Boulet <lucolivierboulet@gmail.com>
2016-06-02 09:30:07 -0400

    Update 'carlito.js' to respect guidelines

--- carlito.js
+++ carlito.js
@@ -1,26 +1,27 @@
 function createButton(title) {
-	var button = document.createElement("a")
-	button.innerText = title
-	button.className = "btn btn-sm"
+  var button = document.createElement("a");
+  button.innerText = title;
+  button.className = "btn btn-sm";
 
-	return button
+  return button;
 }
 
 function appendItem(title, url) {
-	var navigateButton = createButton(title)
-	navigateButton.href = url
-	var element = document.createElement("li")
-	element.appendChild(navigateButton)
+  var navigateButton = createButton(title);
+  navigateButton.href = url;
 
-	document
-		.querySelector(".repohead-details-container .pagehead-actions")
-		.appendChild(element);
+  var element = document.createElement("li");
+  element.appendChild(navigateButton);
+
+  document
+    .querySelector(".repohead-details-container .pagehead-actions")
+    .appendChild(element);
 }
 
-var platform = window.location.pathname.split("-").pop()
+var platform = window.location.pathname.split("-").pop();
 if (["ios", "android"].indexOf(platform) >= 0) {
-	appendItem("Go to core", window.location.href.replace(/(ios|android)$/, "core"))
-} else if (platform == "core") {
-	appendItem("Go to iOS", window.location.href.replace(/core$/, "ios"))
-	appendItem("Go to Android", window.location.href.replace(/core$/, "android"))
+  appendItem("Go to core", window.location.href.replace(/(ios|android)$/, "core"));
+} else if (platform === "core") {
+  appendItem("Go to iOS", window.location.href.replace(/core$/, "ios"));
+  appendItem("Go to Android", window.location.href.replace(/core$/, "android"));
 }
